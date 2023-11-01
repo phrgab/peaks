94881e947c8483e5edd93f9b36a53895d44e56ff
Brendan Edwards <78918760+Brendan-M-Edwards@users.noreply.github.com>
2023-11-01 16:04:49 +0000

    Change cell_above function to make_cell with above/below option

--- peaks/core/utils/__init__.py
+++ peaks/core/utils/__init__.py
@@ -3,6 +3,6 @@
 """
 
 from .consts import consts
-from .misc import make_docs, cell_below
+from .misc import make_docs, make_cell
 from .metadata import update_hist
 from .OOP_method import add_methods
