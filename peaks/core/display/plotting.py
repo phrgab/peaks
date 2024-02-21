36221ce1a77a72a8196d07dd50b1550539629672
Brendan Edwards <78918760+Brendan-M-Edwards@users.noreply.github.com>
2024-02-21 17:01:50 +0000

    Updated doc strings

--- peaks/core/display/plotting.py
+++ peaks/core/display/plotting.py
@@ -13,53 +13,55 @@ from cycler import cycler
 from peaks.core.utils.misc import analysis_warning
 
 
-def plot_grid(data, ncols=3, nrows=None, titles=None, sharex=False, sharey=False, figsize=None, **kwargs):
+def plot_grid(data, ncols=3, nrows=None, titles=None, sharex=False, sharey=False, figsize=None, **plotting_kwargs):
     """Plots an array of 2D DataArrays on a grid.
 
     Parameters
     ------------
     data : list
-         List containing the 2D items (in xr.DataArray format) to be plotted.
+         The 2D items (in :class:`xarray.DataArray` format) to be plotted.
 
-    ncols : int (optional)
+    ncols : int, optional
         Number of columns. Ignored if nrows is specified. Defaults to 3 (or lower if <3 plots).
 
-    nrows : int (optional)
+    nrows : int, optional
         Number of rows. Overwrites ncols. Defaults to None (i.e. ncols is used).
 
-    titles : list (optional)
+    titles : list, optional
         List of subtitles to be supplied on the plots. Length must match length of the data list. Defaults to None.
 
-    sharex : Boolean (optional)
+    sharex : bool, optional
         Whether to have the plots share an x-axis. Defaults to False.
 
-    sharey : Boolean (optional)
+    sharey : bool, optional
         Whether to have the plots share a y-axis. Defaults to False.
 
-    figsize : tuple (optional)
+    figsize : tuple, optional
         Size of figure to be plotted. Defaults to None.
 
-    **kwargs (optional)
+    **plotting_kwargs
         Additional standard matplotlib calls arguments to pass to the plot.
 
     Examples
     ------------
-    from peaks import *
+    Example usage is as follows::
 
-    disp1 = load('disp1.ibw')
-    disp2 = load('disp2.ibw')
-    disp3 = load('disp3.ibw')
-    disp4 = load('disp4.ibw')
+        from peaks import *
 
-    disps_to_plot = [disp1, disp2, disp3, disp4]
-    disp_titles = ['10 K', '20 K', '30 K', '40 K']
+        disp1 = load('disp1.ibw')
+        disp2 = load('disp2.ibw')
+        disp3 = load('disp3.ibw')
+        disp4 = load('disp4.ibw')
 
-    # Plot dispersions on a 2D grid
-    plot_grid(disps_to_plot)
+        disps_to_plot = [disp1, disp2, disp3, disp4]
+        disp_titles = ['10 K', '20 K', '30 K', '40 K']
 
-    # Plot dispersions on a 2D grid, where each dispersion has a title, all dispersions share an x-axis, and
-    # colour bars are not shown
-    plot_grid(disps_to_plot, titles=disp_titles, sharex=True, add_colorbar=False)
+        # Plot dispersions on a 2D grid
+        plot_grid(disps_to_plot)
+
+        # Plot dispersions on a 2D grid, where each dispersion has a title, all dispersions share an x-axis, and
+        # colour bars are not shown
+        plot_grid(disps_to_plot, titles=disp_titles, sharex=True, add_colorbar=False)
 
     """
 
@@ -104,7 +106,7 @@ def plot_grid(data, ncols=3, nrows=None, titles=None, sharex=False, sharey=False
     # Make the plots
     if nrows < 2 or ncols == 1:  # Figure out whether this is a 1D or 2D grid
         for count, value in enumerate(data):
-            value.plot(ax=axes[count], **kwargs)  # Plot data
+            value.plot(ax=axes[count], **plotting_kwargs)  # Plot data
             if plot_titles:  # If plot titles to be displayed, update them here
                 axes[count].set_title(titles[count])
     else:  # 2D grid to display on
@@ -113,7 +115,7 @@ def plot_grid(data, ncols=3, nrows=None, titles=None, sharex=False, sharey=False
         for count, value in enumerate(data):
             j0 = int(np.floor(count / ncols))  # Work out grid positions
             j1 = int(count - ncols * j0)
-            value.plot(ax=axes[j0][j1], **kwargs)  # Plot data
+            value.plot(ax=axes[j0][j1], **plotting_kwargs)  # Plot data
             if plot_titles:  # If plot titles to be displayed, update them here
                 axes[j0][j1].set_title(titles[count])
 
@@ -122,63 +124,64 @@ def plot_grid(data, ncols=3, nrows=None, titles=None, sharex=False, sharey=False
 
 
 def plot_DCs(DCs, titles=None, cmap='coolwarm', color=None, offset=0, norm=False, stack_dim='auto', figsize=(6,6),
-             linewidth=1, ax=None, **kwargs):
+             linewidth=1, ax=None, **plotting_kwargs):
     """Plot a DC stack with the colours varying according to a colormap.
 
     Parameters
     ------------
-    DCs : xr.DataArray, list
-         DCs to plot, either as a list of single DC DataArrays, or a single xr.DataArray with the stack of DCs included.
+    DCs : xarray.DataArray, list
+         DCs to plot, either as a list of single DC DataArrays, or a single DataArray with the stack of DCs included.
 
-    titles : list (optional)
+    titles : list, optional
         List of DC labels to be used in the legend. Length must match number of DCs to plot. Defaults to None.
 
-    cmap : str (optional)
+    cmap : str, optional
         Matplotlib cmap to use for line colors. Defaults to coolwarm.
 
-    color : str (optional)
+    color : str, optional
         Single matplotlib color to use for all lines. Takes precedence over cmap if defined. Defaults to None.
 
-    offset : float (optional)
+    offset : float, optional
         Vertical offsets between subsequent DCs, represented as a fraction of the maximum peak height. Defaults to 0.
 
-    norm : Boolean (optional)
+    norm : bool, optional
         Whether to normalise the DCs to 1. Defaults to False.
 
-    stack_dim : str (optional)
+    stack_dim : str, optional
         Which dimension to stack DCs along. Ignored if a list of DCs is passed. Defaults to 'auto' which takes the
         smallest dimension.
 
-    figsize : tuple (optional)
+    figsize : tuple, optional
         Size of figure to be plotted. Defaults to None.
 
-    linewidth : float (optional)
+    linewidth : float, optional
         Width of lines to be plotted. Defaults to 1.
 
-    ax : numpy.ndarray (optional)
+    ax : numpy.ndarray, optional
         Specific matplotlib axis call. Defaults to None.
 
-    **kwargs (optional)
+    **plotting_kwargs
         Additional standard matplotlib calls arguments to pass to the plot.
 
     Examples
     ------------
-    from peaks import *
+    Example usage is as follows::
 
-    disp1 = load('disp1.ibw')
+        from peaks import *
 
-    EDCs_to_plot = disp.EDC(k=[0, 0.1, 0.2, 0.3, 0.4, 0.5], dk=0.01)
+        disp1 = load('disp1.ibw')
 
-    # Plot EDCs
-    plot_DCs(to_plot)
+        EDCs_to_plot = disp.EDC(k=[0, 0.1, 0.2, 0.3, 0.4, 0.5], dk=0.01)
 
-    MDC_1 = disp.MDC(E=95.61, dE=0.01)
-    MDC_2 = disp.MDC(E=95.62, dE=0.01)
-    MDC_3 = disp.MDC(E=95.63, dE=0.01)
+        # Plot EDCs
+        plot_DCs(to_plot)
 
+        MDC_1 = disp.MDC(E=95.61, dE=0.01)
+        MDC_2 = disp.MDC(E=95.62, dE=0.01)
+        MDC_3 = disp.MDC(E=95.63, dE=0.01)
 
-    # Plot normalised MDCs with a 5% (of maximum height) offset, and adds legend
-    plot_DCs([MDC_1, MDC_2, MDC_3], titles = ['EF', 'EF - 0.01 eV', 'EF -0.02 eV'], norm=True, offset=0.05)
+        # Plot normalised MDCs with a 5% (of maximum height) offset, and add legend
+        plot_DCs([MDC_1, MDC_2, MDC_3], titles = ['EF', 'EF - 0.01 eV', 'EF -0.02 eV'], norm=True, offset=0.05)
 
     """
 
@@ -222,27 +225,27 @@ def plot_DCs(DCs, titles=None, cmap='coolwarm', color=None, offset=0, norm=False
 
     # If no tiles are provided, no legend will be displayed
     if not titles:
-        kwargs['add_legend'] = False
+        plotting_kwargs['add_legend'] = False
 
     # If x and y are not defined, make a guess for which axes to plot the dimensions on.
-    if 'y' not in kwargs and 'x' not in kwargs:
+    if 'y' not in plotting_kwargs and 'x' not in plotting_kwargs:
         # If it seems to be an EDC plot, set vertical by default
         if other_dim == 'eV':
-            kwargs['y'] = 'eV'
+            plotting_kwargs['y'] = 'eV'
         # For anything else, set horizontal
         else:
-            kwargs['x'] = other_dim
+            plotting_kwargs['x'] = other_dim
 
     # Set up plot
     if ax:
-        kwargs['ax'] = ax
+        plotting_kwargs['ax'] = ax
     else:
         plt.figure(figsize=figsize)
         ax = plt.gca()
 
     # If the user has requested to plot lines of a single color
     if color:
-        DC_array.plot.line(hue=stack_dim, linewidth=linewidth, color=color, **kwargs)
+        DC_array.plot.line(hue=stack_dim, linewidth=linewidth, color=color, **plotting_kwargs)
 
     # Else use a colormap
     else:
@@ -251,10 +254,10 @@ def plot_DCs(DCs, titles=None, cmap='coolwarm', color=None, offset=0, norm=False
         cmap = colors.ListedColormap(cols)
         custom_cycler = cycler(color=cmap.colors)
         ax.set_prop_cycle(custom_cycler)
-        DC_array.plot.line(hue=stack_dim, linewidth=linewidth, **kwargs)
+        DC_array.plot.line(hue=stack_dim, linewidth=linewidth, **plotting_kwargs)
 
     # Add axis titles
-    if 'y' in kwargs:
+    if 'y' in plotting_kwargs:
         plt.xlabel("Intensity [arb. units]")
     else:
         plt.ylabel("Intensity [arb. units]")
@@ -274,7 +277,7 @@ def plot_DCs(DCs, titles=None, cmap='coolwarm', color=None, offset=0, norm=False
     plt.tight_layout()
 
 
-def plot_ROI(ROI, color='black', x=None, y=None, label=None, loc='best', ax=None, **kwargs):
+def plot_ROI(ROI, color='black', x=None, y=None, label=None, loc='best', ax=None, **plotting_kwargs):
     """This function plots a region of interest (ROI).
 
     Parameters
@@ -284,53 +287,55 @@ def plot_ROI(ROI, color='black', x=None, y=None, label=None, loc='best', ax=None
         {'dim1': [pt1, pt2, pt3, ...], 'dim2'=[pt1', pt2', pt3', ...]}. As many points can be specified as required,
         but this should be given with the same number of points for each dimension.
 
-    color : str (optional)
+    color : str, optional
         Matplotlib color to use for ROI plot. Defaults to 'black'.
 
-    x : str (optional)
+    x : str, optional
         Specifies which coordinate to plot along the x-axis. Takes precedence over y (only one of x and y need to be
         specified, the other is set automatically). Defaults to the first coordinate listed in the ROI dictionary.
 
-    y : str (optional)
+    y : str, optional
         Specifies which coordinate to plot along the y-axis. Defaults to the second coordinate listed in the ROI
         dictionary.
 
-    label : str (optional)
+    label : str, optional
         ROI label to pass to a legend. Defaults to None (where no legend is plotted).
 
-    loc : str, int (optional)
+    loc : str, int, optional
         Standard matplotlib call to specify legend location. Defaults to 'best'.
 
-    ax : numpy.ndarray (optional)
+    ax : numpy.ndarray, optional
         Specific matplotlib axis call. Defaults to None.
 
-    **kwargs (optional)
+    **plotting_kwargs
         Additional standard matplotlib calls arguments to pass to the plot.
 
     Examples
     ------------
+    Example usage is as follows::
+
     from peaks import *
 
-    disp = load('disp1.ibw')
+        disp = load('disp1.ibw')
 
-    ROI1 = {'theta_par': [-8, -5.5, -3.1, -5.6], 'eV': [95.45, 95.45, 95.77, 95.77]}
-    ROI2 = {'theta_par': [-9, -4, -2.5, -5.9], 'eV': [95.45, 95.45, 95.77, 95.77]}
+        ROI1 = {'theta_par': [-8, -5.5, -3.1, -5.6], 'eV': [95.45, 95.45, 95.77, 95.77]}
+        ROI2 = {'theta_par': [-9, -4, -2.5, -5.9], 'eV': [95.45, 95.45, 95.77, 95.77]}
 
-    # Plot ROI
-    plot_ROI(ROI1)
-    plt.show()
+        # Plot ROI
+        plot_ROI(ROI1)
+        plt.show()
 
-    # Plot ROI on top of a dispersion with eV plotted along the x-axis.
-    disp1.plot()
-    plot_ROI(ROI1, x='eV')
-    plt.show()
+        # Plot ROI on top of a dispersion with eV plotted along the x-axis.
+        disp1.plot()
+        plot_ROI(ROI1, x='eV')
+        plt.show()
 
-    # Plot ROIs on top of dispersions on specific axes, axes[0] and axes[1], with labels
-    fig, axes= plt.subplots(ncols=2)
-    disp.T.plot(ax=axes[0])
-    plot_ROI(ROI1, label='ROI 1', ax=axes[0])
-    disp.T.plot(ax=axes[1])
-    plot_ROI(ROI2, label='ROI 2', ax=axes[1])
+        # Plot ROIs on top of dispersions on specific axes, axes[0] and axes[1], with labels
+        fig, axes= plt.subplots(ncols=2)
+        disp.T.plot(ax=axes[0])
+        plot_ROI(ROI1, label='ROI 1', ax=axes[0])
+        disp.T.plot(ax=axes[1])
+        plot_ROI(ROI2, label='ROI 2', ax=axes[1])
 
     """
 
@@ -357,14 +362,14 @@ def plot_ROI(ROI, color='black', x=None, y=None, label=None, loc='best', ax=None
     # Plot the ROI
     if ax:  # If particular axis specified
         if yax == 0:
-            ax.plot(verts1, verts0, color=color, label=label, **kwargs)
+            ax.plot(verts1, verts0, color=color, label=label, **plotting_kwargs)
         else:
-            ax.plot(verts0, verts1, color=color, label=label, **kwargs)
+            ax.plot(verts0, verts1, color=color, label=label, **plotting_kwargs)
     else:  # Otherwise just call with plt
         if yax == 0:
-            plt.plot(verts1, verts0, color=color, label=label, **kwargs)
+            plt.plot(verts1, verts0, color=color, label=label, **plotting_kwargs)
         else:
-            plt.plot(verts0, verts1, color=color, label=label, **kwargs)
+            plt.plot(verts0, verts1, color=color, label=label, **plotting_kwargs)
 
     # If called with label, plot the legend
     if label:
