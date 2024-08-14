2227bab007d0c98dfbb4f788cbbffdb38c3a8857
pdk6 <pdk6@st-andrews.ac.uk>
2024-08-14 18:32:44 +0100

    inital attempts at processing - work in progress

--- peaks/core/process/process.py
+++ peaks/core/process/process.py
@@ -1,8 +1,6 @@
-"""Master function used to apply a series of processing operations on data.
+"""Main function used to apply a series of processing operations on data.
 
 """
-
-# Phil King 09/06/2021
 
 
 def proc():
@@ -10,16 +8,4 @@ def proc():
 
 
 def _proc_single():
-    pass
-
-
-def _add_estimated_EF():
-    pass
-
-
-def _add_estimated_hv():
-    pass
-
-
-def _estimate_EF(data, fit=False):
     pass
