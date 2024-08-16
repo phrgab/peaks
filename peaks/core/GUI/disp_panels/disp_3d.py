e1c6d51eb6ddff72e84ba0af9ebf86df9df22f0e
pdk6 <pdk6@st-andrews.ac.uk>
2024-08-16 19:26:35 +0100

    fix(disp_3d) : bug fix for display of hv scans

--- peaks/core/GUI/disp_panels/disp_3d.py
+++ peaks/core/GUI/disp_panels/disp_3d.py
@@ -620,7 +620,10 @@ class _Disp3D(QtWidgets.QMainWindow):
         # If eV in data, attempt to set a Fermi level and use this as initial xh pos
         if "eV" in self.dims:
             eV_dim = self.data.dims.index("eV")
-            if self.data.attrs.get("scan_type") == "hv scan" and "hv" in self.dims:
+            if (
+                self.data.attrs.get("scan_type") == "hv scan"
+                and self.data.attrs.get("eV_type") == "kinetic"
+            ):
                 # Due to the particular data structure of an hv scan, take the central slice to estimate EF
                 hv = self.data.hv.data
                 middle_hv = hv[(len(hv) - 1) // 2]
