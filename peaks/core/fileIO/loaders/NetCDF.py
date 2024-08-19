764325123399c1a305883cfcc007fa1dc2496ccb
pdk6 <pdk6@st-andrews.ac.uk>
2024-08-19 15:35:05 +0100

    feat(data_saving) : update netCDF to work with new LocOpts, and add proper analysis_history parsing

--- peaks/core/fileIO/loaders/NetCDF.py
+++ peaks/core/fileIO/loaders/NetCDF.py
@@ -2,10 +2,10 @@
 
 """
 
-# Brendan Edwards 16/02/2021
-# Brendan Edwards 09/02/2024
-
 import xarray as xr
+import json
+
+from ..fileIO_opts import _BaseDataConventions, _register_location
 
 
 def _load_NetCDF_data(fname):
@@ -40,19 +40,29 @@ def _load_NetCDF_data(fname):
     # Since NetCDF file attributes cannot be saved as None type, we want to revert any empty strings to type None
     # (except for analysis_history which if it happens to be empty, we want it to be an empty list)
     for attr in data.attrs:
-        if data.attrs[attr] == '':
-            if attr != 'analysis_history':
+        if data.attrs[attr] == "":
+            if attr != "analysis_history":
                 data.attrs[attr] = None
             else:
                 data.attrs[attr] = []
 
-    # Since NetCDF file attributes cannot be saved as a list, we represent the list analysis_history as a string with
-    # items seperated by the identifier '<->' when saved in a NetCDF file. We want to revert analysis_history to a list.
+    # Since NetCDF file attributes cannot be saved as a list, we represent the list analysis_history as a json string.
+    # We want to revert analysis_history to a list.
     try:  # If data.attrs['analysis_history'] is a string, split it into a list
-        data.attrs['analysis_history'] = data.attrs['analysis_history'].split('<->')[1:]
-    except AttributeError:  # If data.attrs['analysis_history'] is not a string, do nothing (future proofing)
+        data.attrs["analysis_history"] = json.loads(data.attrs["analysis_history"])
+    except (
+        AttributeError
+    ):  # If data.attrs['analysis_history'] is not a string, do nothing (future proofing)
         pass
-    except KeyError:  # If data.attrs['analysis_history'] does not exist, define it with no history
-        data.attrs['analysis_history'] = []
+    except (
+        KeyError
+    ):  # If data.attrs['analysis_history'] does not exist, define it with no history
+        data.attrs["analysis_history"] = []
 
     return data
+
+
+@_register_location
+class _NetCDF(_BaseDataConventions):
+    loc_name = "NetCDF"
+    loader = _load_NetCDF_data
