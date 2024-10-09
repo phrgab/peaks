195ddb5d3f521c76d16d135464ce98c437858843
phrgab <78075132+phrgab@users.noreply.github.com>
2024-10-09 07:01:57 +0100

    Added method for quick printing of hdf5 structure

--- peaks/core/fileIO/__init__.py
+++ peaks/core/fileIO/__init__.py
@@ -8,4 +8,4 @@ from .loc_registry import LOC_REGISTRY
 from .data_loading import load, _load_data
 from .data_saving import save
 from .fileIO_opts import File, LocOpts
-from .helper_functions import make_hv_scan, slant_correct
+from .helper_functions import make_hv_scan, slant_correct, hdf5_explorer
