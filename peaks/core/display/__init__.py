81089132f09c7a27b011473edfcf51b81799826a
phrgab <78075132+phrgab@users.noreply.github.com>
2025-03-07 08:48:12 +0000

    add methods for extracting and plotting cut along slit

--- peaks/core/display/__init__.py
+++ peaks/core/display/__init__.py
@@ -1,5 +1,10 @@
-"""Core functionality for in-line data display.
+"""Core functionality for in-line data display."""
 
-"""
-
-from .plotting import plot_grid, plot_DCs, plot_ROI, plot_nanofocus
+from .plotting import (
+    plot_DCs,
+    plot_grid,
+    plot_kpar_cut,
+    plot_kz_cut,
+    plot_nanofocus,
+    plot_ROI,
+)
