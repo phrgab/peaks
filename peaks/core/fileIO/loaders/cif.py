8d29effa1134dcf99b50dbd734a43f669cd0e2fa
phrgab <78075132+phrgab@users.noreply.github.com>
2025-03-23 15:56:03 +0000

    refactor: move ase import check into the load method for optional depenency

--- peaks/core/fileIO/loaders/cif.py
+++ peaks/core/fileIO/loaders/cif.py
@@ -1,11 +1,3 @@
-try:
-    import ase.io
-except ImportError as e:
-    raise ImportError(
-        "The 'ase' module is required to use this module. Please install it using \
-'pip install ase'."
-    ) from e
-
 from peaks.core.fileIO.base_data_classes.base_data_class import BaseDataLoader
 from peaks.core.fileIO.loc_registry import register_loader
 
@@ -19,5 +11,13 @@ class BaseStructureLoader(BaseDataLoader):
     @classmethod
     def _load(cls, fpath, lazy, metadata, quiet, **kwargs):
         """Load the structure data."""
+
+        try:
+            import ase.io
+        except ImportError as e:
+            raise ImportError(
+                "The 'ase' module is required to use this module. Please install it using \
+        'pip install ase'."
+            ) from e
 
         return ase.io.read(fpath)
